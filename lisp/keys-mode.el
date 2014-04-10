3a608f8ac9d23fc40718e3b3d2b4cd770a22285a
ramnes <contact@ramnes.eu>
2014-04-10 20:24:30 +0100

    Update keys-mode

--- lisp/keys-mode.el
+++ lisp/keys-mode.el
@@ -9,22 +9,26 @@
 (define-key keys-mode-map (kbd "C-c C-r") 'replace-regexp)
 (define-key keys-mode-map (kbd "C-c C-s") 'replace-string)
 
+(define-key keys-mode-map (kbd "C-f") 'fill-region)
 (define-key keys-mode-map (kbd "C-l") 'goto-line)
+(define-key keys-mode-map (kbd "C-v") 'sort-lines)
 
 (define-key keys-mode-map (kbd "C-x C-b") 'buffer-menu)
 (define-key keys-mode-map (kbd "C-x C-k") 'kill-buffer-and-window)
 (define-key keys-mode-map (kbd "C-x b") 'buffer-menu)
 (define-key keys-mode-map (kbd "C-x k") 'kill-buffer)
 
-(define-key keys-mode-map (kbd "<M-left>") 'windmove-left)
-(define-key keys-mode-map (kbd "<M-right>") 'windmove-right)
-(define-key keys-mode-map (kbd "<M-up>") 'windmove-up)
-(define-key keys-mode-map (kbd "<M-down>") 'windmove-down)
+(define-key keys-mode-map (kbd "M-<down>") 'windmove-down)
+(define-key keys-mode-map (kbd "M-<left>") 'windmove-left)
+(define-key keys-mode-map (kbd "M-<right>") 'windmove-right)
+(define-key keys-mode-map (kbd "M-<up>") 'windmove-up)
 
+;; dependent of move-border
+;; https://github.com/ramnes/move-border
+(define-key keys-mode-map (kbd "M-S-<down>") 'move-border-down)
 (define-key keys-mode-map (kbd "M-S-<left>") 'move-border-left)
 (define-key keys-mode-map (kbd "M-S-<right>") 'move-border-right)
 (define-key keys-mode-map (kbd "M-S-<up>") 'move-border-up)
-(define-key keys-mode-map (kbd "M-S-<down>") 'move-border-down)
 
 (define-minor-mode keys-mode
   "Overriding key bindings" t " Keys" 'keys-mode-map)
