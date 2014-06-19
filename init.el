d47e2004739b780f7424a261fe195e6dadc33def
ramnes <contact@ramnes.eu>
2014-06-19 12:42:43 +0200

    Make Flymake working against symlinks

--- init.el
+++ init.el
@@ -15,14 +15,12 @@
 (load "init-theme")
 
 ;; Flymake (from http://www.plope.com/Members/chrism/flymake-mode)
+;; updated because of symlinks messing up: http://bit.ly/1pipbJb
 (when (load "flymake" t)
   (defun flymake-pyflakes-init ()
     (let* ((temp-file (flymake-init-create-temp-buffer-copy
-                       'flymake-create-temp-inplace))
-           (local-file (file-relative-name
-                        temp-file
-                        (file-name-directory buffer-file-name))))
-      (list "pyflakes" (list local-file))))
+                       'flymake-create-temp-inplace)))
+      (list "pyflakes" (list temp-file))))
   (add-to-list 'flymake-allowed-file-name-masks
                '("\\.py\\'" flymake-pyflakes-init)))
 (add-hook 'find-file-hook 'flymake-find-file-hook)
