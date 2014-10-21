0ce48d2b123dc1572815c80366937a3565da8bc0
Guillaume Gelin <ramnes@1000mercis.com>
2014-10-21 10:55:05 +0200

    Emacs 24.4 doesn't like .emacs.d in the load-path

--- init.el
+++ init.el
@@ -4,9 +4,10 @@
 ;;
 
 ;; Load path
-(let ((default-directory "~/.emacs.d/"))
+(let ((default-directory "~/.emacs.d/site-lisp/"))
   (normal-top-level-add-to-load-path '("."))
   (normal-top-level-add-subdirs-to-load-path))
+(add-to-list 'load-path (expand-file-name "~/.emacs.d/lisp"))
 
 ;; Load lisp
 (load "init-requires")
