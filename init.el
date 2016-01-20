62ec8ecf66a96219166054d46858780c1ea1cfeb
Guillaume Gelin <ramnes@1000mercis.com>
2016-01-20 15:47:47 +0100

    Improve desktop-read

--- init.el
+++ init.el
@@ -197,3 +197,8 @@
 
 ;; Save commands history
 (savehist-mode 1)
+
+;; Improve desktop-read
+(setq desktop-restore-frames t)
+(setq desktop-restore-in-current-display t)
+(setq desktop-restore-forces-onscreen nil)
