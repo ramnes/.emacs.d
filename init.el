a239ca411b80abba0db678d53ef58a9a2ceaed7c
Guillaume Gelin <ramnes@1000mercis.com>
2017-09-21 16:10:19 +0200

    Restrict on the 79th column to comply with PEP8,

--- init.el
+++ init.el
@@ -61,9 +61,11 @@
 (autopair-global-mode 1)
 
 ;; 80 char rule (for text and prog only)
-(setq-default fill-column 80)
+(setq-default fill-column 79)
 (add-hook 'text-mode-hook 'auto-fill-mode)
 (add-hook 'prog-mode-hook 'auto-fill-mode)
+
+(setq column-enforce-column 79)
 (add-hook 'text-mode-hook 'column-enforce-mode)
 (add-hook 'prog-mode-hook 'column-enforce-mode)
 
