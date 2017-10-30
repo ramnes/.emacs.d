91de33fde7e175e7b321eafcc9ec4cacb5bd5c08
Guillaume Gelin <ramnes@1000mercis.com>
2017-10-30 18:07:57 +0100

    go-mode doesn't ship with pre-generated autoloads anymore

--- init.el
+++ init.el
@@ -10,6 +10,11 @@
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/lisp"))
 (setq exec-path (append exec-path '("~/.emacs.d/bin")))
 
+;; Create go-mode-autoloads if it doesn't exist yet
+(if (not (file-exists-p "~/.emacs.d/site-lisp/go-mode/go-mode-autoloads.el"))
+    (update-file-autoloads "~/.emacs.d/site-lisp/go-mode/go-mode.el" t
+                           "~/.emacs.d/site-lisp/go-mode/go-mode-autoloads.el"))
+
 ;; Load lisp
 (load "init-requires")
 (load "init-funcs")
