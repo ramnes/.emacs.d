cc608fdfcdff1f6ca6b809115fcf3b7879dcbeb3
ramnes <contact@ramnes.eu>
2024-09-02 14:40:06 +0200

    Use mardown-mode for .mdx

--- init.el
+++ init.el
@@ -62,6 +62,7 @@
 ;; Markdown-mode
 (add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.mdx\\'" . markdown-mode))
 (setq markdown-fontify-code-blocks-natively t)
 
 ;; Web-mode
